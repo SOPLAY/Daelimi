efadbe0127d5f07c001e2065622aa8bda6e0054e
tkdgns1341 <tkdgns1341@gmail.com>
2022-02-25 14:17:46 +0900

    [ Splash ] SVG, PNG Size change svg와 png 파일의 사이즈 변경

--- components/Splash.js
+++ components/Splash.js
@@ -1,9 +1,14 @@
 import Image from "next/image";
-import LogoImg from "../public/ico.png";
 export default function Splash() {
   return (
-    <div className="flex justify-center items-center  bg-blue-100 h-full ">
-      <Image src={LogoImg} alt="로고이미지" width={100} height={100} />
+    <div className="flex justify-center items-center  bg-white h-full ">
+      <Image
+        src="/image/univLogo.png"
+        alt="로고이미지"
+        width={250}
+        height={63}
+      />
+      <Image src="/image/ico.svg" alt="로고이미지" width={63} height={63} />
     </div>
   );
 }
