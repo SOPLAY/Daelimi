ac0aa15abfb167ede2546e31a2c1648b574cbe3b
tkdgns1341 <tkdgns1341@gmail.com>
2022-02-27 02:04:38 +0900

    [ Component Separation ] (index.js => index.js + components/ChatingView.js ) 컴포넌트 분리 index.js 의 chating 컴포넌트 생성 및 분리

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,11 @@
-// import SendMessage from "../components/SendMessage";
-import { useRecoilValue } from "recoil";
+import ChatingView from "../components/ChatingView";
 import MessageBox from "../components/InputMessageBox";
-import { chatLogsAtom } from "../core/atom/atomChatLogs";
+
 export default function Home() {
-  let chatlog = useRecoilValue(chatLogsAtom);
   return (
     <>
-      <div className=" chatLogContainer justify-center h-[85%] pt-1 pb-4 ">
-        {/* 오버플로 발생시 아래 메시지 고정 및 정렬 */}
-        <div className="flex flex-col-reverse overflow-auto h-full">
-          <div className="px-5 ">
-            {chatlog.map((value, index) => (
-              <div
-                key={index}
-                className={`dark:text-white duration-500 flex ${
-                  value.user && "justify-end"
-                }`}
-              >
-                <div className="text-xl">{value.message}</div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
+      <ChatingView />
       <MessageBox />
     </>
   );
 }
-
-// export default function test() {
-//   return <Splash />;
-// }
