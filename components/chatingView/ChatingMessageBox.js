6f121bbb2dc99d96b4f3de109f312d5a0fa2a60e
tkdgns1341 <tkdgns1341@gmail.com>
2022-02-27 15:14:15 +0900

    [ chatMessageBox Styling , html min-w-max Setting ] ( components/chatingView/ChatingMessageBox.js , pages/_documents.js ) 채팅 박스 세부 디자인 설정 및 최소 사이즈 설정

--- components/chatingView/ChatingMessageBox.js
+++ components/chatingView/ChatingMessageBox.js
@@ -1,20 +1,24 @@
 import Logo from "../../public/image/ico.svg";
 export default function ChatingMessageBox({ user, message, time }) {
   return (
-    <div className={`flex mt-2 ml-i mb-2 ${user && "justify-end"}`}>
+    <div className={`flex mt-3 md:mt-8 ml-i mb-2 ${user && "justify-end"}`}>
       <div className={`flex ${user || "flex-row-reverse"}`}>
         <div className={`flex justify-center items-end mx-2 mb-2 text-gray-400 text-xs`}>
           {time}
         </div>
 
         <div
-          className={`max-w-xs rounded-md p-2 shadow-lg ${user ? "bg-yellow-200" : "bg-blue-200"} `}
+          className={`text-sm md:text-base md:max-w-md rounded-md p-2 shadow-md ${
+            user
+              ? "max-w-[250px] bg-yellow-200 shadow-yellow-500/30"
+              : "max-w-[220px] bg-blue-200 shadow-blue-500/30 "
+          } `}
         >
           {message}
         </div>
 
         {user || (
-          <div className="flex justify-center items-start ">
+          <div className="flex justify-center items-start mr-1 md:mr-2">
             <Logo width={40} height={40} />
           </div>
         )}
