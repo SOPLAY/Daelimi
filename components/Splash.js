97c3b9e53d384d61d974b1207dffbeaad3d1a6ff
tkdgns1341 <tkdgns1341@gmail.com>
2022-02-27 16:11:06 +0900

    [ UserFilter Req Change , logo size renew ] (core/filter/userInpoutFilter.js , components/Splash.js , public/image/unveLogo.png ) 필터에 중복값 제거 및 대림대 로고 사이즈 변경

--- components/Splash.js
+++ components/Splash.js
@@ -2,12 +2,7 @@ import Image from "next/image";
 export default function Splash() {
   return (
     <div className="flex justify-center items-center h-full bg-white dark:bg-neutral-900">
-      <Image
-        src="/image/univLogo.png"
-        alt="로고이미지"
-        width={250}
-        height={63}
-      />
+      <Image src="/image/univLogo.png" alt="로고이미지" width={236.25} height={63} />
       <Image src="/image/ico.svg" alt="로고이미지" width={63} height={63} />
     </div>
   );
