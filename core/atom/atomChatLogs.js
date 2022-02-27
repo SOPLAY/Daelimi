881dd303e48c0528ad6520770468fc9a4f4bccfd
tkdgns1341 <tkdgns1341@gmail.com>
2022-02-27 12:50:55 +0900

    [ title-meta ADD , ico,png change, ChatLogsAtom key chang ] (page/_app.js, public/**/* , core/atom/atomChatLogs.js ) 메타데이터 타이틀 추가 이아콘 관련 파일들 변경및 불필요 파일 제거 recoil Atom key 변경

--- core/atom/atomChatLogs.js
+++ core/atom/atomChatLogs.js
@@ -1,6 +1,6 @@
 import { atom } from "recoil";
 
 export const chatLogsAtom = atom({
-  key: "chatLogs",
+  key: "chatLogsAtom",
   default: [],
 });
--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { useEffect, useState } from "react";
 import { RecoilRoot } from "recoil";
 import Header from "../components/Header";
@@ -18,17 +19,42 @@ function MyApp({ Component, pageProps }) {
         document.documentElement.classList.add("scroll-smooth");
       }, 1000);
   }, [lodingSplash]);
-  return lodingSplash ? (
-    <Splash />
-  ) : (
-    <div className=" h-full w-full dark:bg-neutral-800 duration-500 ">
-      <div className=" h-full mx-auto max-w-2xl box-content ">
-        <Header HeaderTitle={"Daelimi"} />
-        <RecoilRoot>
-          <Component {...pageProps} />
-        </RecoilRoot>
-      </div>
-    </div>
+  const headerData = {
+    title: "대림이",
+    description: "대림대학교에 대한 질문사항을 검색해 보세요!",
+    image: "/favicon.png",
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Daelimi</title>
+        {/* 메타 태그 */}
+        <meta name="apple-mobile-web-app-title" content="Daelimi" />
+        <meta name="description" content={headerData.description} />
+        {["og", "twitter"].map((metaMain, index1) =>
+          ["title", "url", "image", "description"].map((value, index2) => (
+            <meta
+              name={`${metaMain}:${value}`}
+              content={headerData[value]}
+              key={index1 * 10 + index2}
+            />
+          ))
+        )}
+      </Head>
+      {lodingSplash ? (
+        <Splash />
+      ) : (
+        <div className=" h-full w-full dark:bg-neutral-800 duration-500 ">
+          <div className=" h-full mx-auto max-w-2xl box-content ">
+            <Header HeaderTitle={"Daelimi"} />
+            <RecoilRoot>
+              <Component {...pageProps} />
+            </RecoilRoot>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
